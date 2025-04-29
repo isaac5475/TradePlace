51af428573b33cf1b8ae33eaca7f34fd15026b67
murat <murat.zaydullin@student.uts.edu.au>
2025-04-29 13:09:05 +1000

    Initial Commit

---
+++ TradePlace/TradePlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradePlaceApp.swift
+//  TradePlace
+//
+//  Created by Murat Zaydullin on 29/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct TradePlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TradePlaceTests/TradePlaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  TradePlaceTests.swift
+//  TradePlaceTests
+//
+//  Created by Murat Zaydullin on 29/4/2025.
+//
+
+import Testing
+@testable import TradePlace
+
+struct TradePlaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
