4dd45dbf1b8c4ed0b26e9b2a2877ace9402e500f
Murat Zaydullin <murat.zaydullin@student.uts.edu.au>
2025-05-12 14:49:16 +1000

    fetching images

--- TradePlace/ViewModels/YourItemsViewModel.swift
+++ TradePlace/ViewModels/YourItemsViewModel.swift
@@ -25,54 +25,10 @@ class YourItemsViewModel : ObservableObject {
     init() {
         Task {
             await fetchItems();
-            await fetchItemImages();
         }
     }
     
-    func fetchItemImages() async {
-        for index in items.indices {
-            var itemImages = [Image]()
-            
-            let storage = Storage.storage()
-            let storageRef = storage.reference()
-            let itemFolderRef = storageRef.child("\(items[index].id.uuidString)");
-            
-            do {
-                let fetchedImages = try await itemFolderRef.listAll()
-                //print("Downloading images for \(items[index].id.uuidString)")
-
-                    for imageRef in fetchedImages.items {
-                        //print("Downloading...")
-                        do {
-                            let data = try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Data, Error>) in
-                                imageRef.getData(maxSize: 2 * 1024 * 1024) { data, error in
-                                    if let error = error {
-                                        continuation.resume(throwing: error)
-                                    } else if let data = data {
-                                        continuation.resume(returning: data)
-                                    }
-                                }
-                            }
-
-                            if let uiImage = UIImage(data: data) {
-                                itemImages.append(Image(uiImage: uiImage))
-                                //print("Added image to itemImages")
-                            }
-
-                        } catch {
-                            print("Error downloading image: \(error)")
-                        }
-                    }
-
-                } catch {
-                    print("Error: could not get item images from Firebase storage: \(error)")
-                }
-                //print("we have got the images for \(items[index].title)!")
-                var updatedItem = items[index]
-                updatedItem.images = itemImages
-                items[index] = updatedItem
-            }
-        }
+    
     
     func fetchItems() async {
         let db = Firestore.firestore()
@@ -80,10 +36,9 @@ class YourItemsViewModel : ObservableObject {
             let itemsForUser = try await db.collection("Users").document(Utils.uuid(from: user.uid).uuidString).collection("TradeItems").getDocuments()
             var items : [TradeItem] = []
             for document in itemsForUser.documents {
-                let data = document.data()
-                let tradeItem = await TradeItem.parseTradeItem(tradeItemId: UUID(uuidString: document.documentID)!, data)
-                if tradeItem != nil {
-                    items.insert(tradeItem!, at: 0)
+                let tradeItem = await TradeItem.fetchTradeItem(document.reference, fetchImages: true)
+                if let tradeItem = tradeItem {
+                    items.append(tradeItem)
                 } else {
                     continue;
                 }
